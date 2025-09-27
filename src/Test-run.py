a4b52eb7058860b0fdca4d313916ec60803ef8c9
Екатерина <90092671+belkatya@users.noreply.github.com>
2025-09-27 19:48:59 +0300

    Add files via upload

---
+++ src/Test-run.py
@@ -0,0 +1,10 @@
+from main import run_agent
+
+
+# Пример использования функции
+response = run_agent("Какова моя цифровая зрелость?")
+print(response)
+response = run_agent("Предложи мне необходимые для моей компании ИТ-решения")
+print(response)
+response = run_agent("Ты можешь дать ссылки на конкретные сайты продуктов Облачные сервисы? если да, то напиши")
+print(response)
